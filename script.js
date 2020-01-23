c77f25c776ea4abc1c7364011bf47e1f3ca2f0c4
dontelivingston <60226931+dontelivingston@users.noreply.github.com>
2020-01-23 08:52:12 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document ).ready(function() {
+    $(".JCPsign").hide();
+});
+$("button").click(function() {
+    $("https://nobleschools.org/wp-content/uploads/2018/03/noble-seal-150px.png").show();
+});
